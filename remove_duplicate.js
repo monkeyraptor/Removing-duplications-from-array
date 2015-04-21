b549173973734a5950933ffeea04b12f73b4c9a0
Monkey Raptor <paul.johanov@gmail.com>
2015-04-21 11:56:04 +0700

    Update remove_duplicate.js

--- remove_duplicate.js
+++ remove_duplicate.js
@@ -1,44 +1,48 @@
-function r_d(a) {  
-  var b = a.slice(), //copy array
-      i = 0, flag = 1,
-      length = b.length,
-      j;
-  
-  //splicing same elements
-  //this has two dimensional tracing. Pairing.
-  for (i; i < length; i++) {
-    j = 0;
-    for (j; j < length; j++) {         
-      if (i < j && b[j] === b[i]) {
-        b.splice(j, 1);
-      }
-    }
-  }
-  
-  //re-test SAME ELEMENTS
-  //"flag switcher"
-  function check(b, f) {
-    var m = 0,
-        n;
-    //"length" is a global variable within this function, "r_d"
-    //it will dynamically change if the array gets spliced
-    for (m; m < length; m++) {
-      n = 0;
-      for (n; n < length; n++) {      
-        if (m < n && b[m] === b[n]) {
-          f = 0; //same elements found
-          break;
+function r_d(a) {
+    "use strict";
+    var b = a.slice(), //copy array
+        i = 0,
+        flag = 1,
+        length = b.length,
+        j;
+
+    //splicing same elements
+    //this has two dimensional tracing. Pairing.
+    for (i; i < length; i++) {
+        j = 0;
+        for (j; j < length; j++) {
+            if (i < j && b[j] === b[i]) {
+                b.splice(j, 1);
+            }
         }
-      }
     }
-    
-    //return flag
-    return f;
-  }
-  
-  //if flag is false, repeat from the beginning
-  if (!check(b, flag)) { b = r_d(b); }
-  
-  //return reconstructed array
-  return b;
+
+    //re-test SAME ELEMENTS
+    //"flag switcher"
+    function check(b, f) {
+        var m = 0,
+            n;
+        //"length" is a global variable within this function, "r_d"
+        //it will dynamically change if the array gets spliced
+        for (m; m < length; m++) {
+            n = 0;
+            for (n; n < length; n++) {
+                if (m < n && b[m] === b[n]) {
+                    f = 0; //same elements found
+                    break;
+                }
+            }
+        }
+
+        //return flag
+        return f;
+    }
+
+    //if flag is false, repeat from the beginning
+    if (!check(b, flag)) {
+        b = r_d(b);
+    }
+
+    //return reconstructed array
+    return b;
 }
