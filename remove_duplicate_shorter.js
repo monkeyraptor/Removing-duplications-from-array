60bb2d43af83c367b891dda8702bcb0b39201524
Monkey Raptor <paul.johanov@gmail.com>
2015-04-22 11:00:46 +0700

    Create remove_duplicate_shorter.js

---
+++ remove_duplicate_shorter.js
@@ -0,0 +1,27 @@
+function r_d_2(a) {
+    "use strict";
+    var b = a.slice(), //copy array
+        i = 0,
+        k = 0,
+        length = b.length,
+        j,
+        buffer = [];
+
+    //splicing same elements
+    for (i; i < length; i++) {
+        j = 0;
+        for (j; j < length; j++) {
+            if (i < j && b[j] === b[i]) {
+                b.splice(j, 1, "deleted");
+            }
+        }
+    }
+    //removing "deleted" elements
+    for (k; k < length; k++) {
+        if (b[k] !== "deleted") {
+            buffer.push(b[k]);
+        }
+    }
+    //return reconstructed array
+    return buffer;
+}
