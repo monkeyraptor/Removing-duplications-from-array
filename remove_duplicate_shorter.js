8a4cbd8cc451fae5fbdfd3a9b507abc03f426cdb
Monkey Raptor <paul.johanov@gmail.com>
2015-05-10 21:21:18 +0700

    Further shortened
    Switched *for*s to *forEach*.

--- remove_duplicate_shorter.js
+++ remove_duplicate_shorter.js
@@ -1,27 +1,21 @@
-function r_d(a) {
+var r_d = function (a) {
     "use strict";
-    var b = a.slice(), //copy array
-        buffer = [],
-        i = 0,
-        j,
-        k = 0,
-        length = b.length;
-
-    //splicing same elements
-    for (i; i < length; i++) {
-        j = 0;
-        for (j; j < length; j++) {
-            if (i < j && b[j] === b[i]) {
-                b.splice(j, 1, b[j] + " (deleted)"); //substitute the same element with "that_elm (deleted)" token
+    var b,
+        buffer = [];
+    if (a instanceof Array) {
+        b = a.slice();
+        b.forEach(function (v_i, i_i) {        
+            b.forEach(function (v_j, i_j) {
+                if (i_i < i_j && v_j === v_i) {
+                    b.splice(i_j, 1, v_j + " (*)");
+                }
+            });
+        });
+        b.forEach(function (v) {    
+            if (!v.toString().match(/\(\*\)/g)) {
+                buffer.push(v);
             }
-        }
+        });
     }
-    //take the non-"deleted" elements to the *buffer* variable
-    for (k; k < length; k++) {
-        if (!b[k].toString().match("(deleted)")) { //if it doesn't match the token: "(deleted)"
-            buffer.push(b[k]);
-        }
-    }
-    //return reconstructed array
     return buffer;
-}
+};
