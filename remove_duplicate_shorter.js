b00399c3bc54f0748265e417d127546446b5556d
Monkey Raptor <paul.johanov@gmail.com>
2015-09-02 04:22:33 +0700

    Updated the marking
    and some others.

--- remove_duplicate_shorter.js
+++ remove_duplicate_shorter.js
@@ -1,21 +1,20 @@
-var r_d = function (a) {
+function r_d(a) {
     "use strict";
-    var b,
-        buffer = [];
+    var b, buffer = [];
     if (a instanceof Array) {
-        b = a.slice(); // Copy array.
-        b.forEach(function (v_i, i_i) { // 2D array iteration - parent (outer) block.
-            b.forEach(function (v_j, i_j) { // 2D array iteration - child (inner) block.
-                if (i_i < i_j && v_j === v_i) { // Can also: if (i_i !== i_j && v_j === v_i) { ...
-                    b.splice(i_j, 1, v_j + " (*)"); // The " (*)" is the "same element" marker.
+        b = a.slice();
+        b.forEach(function (v, i) {
+            b.forEach(function (vv, ii) {
+                if (i !== ii && v === vv) {
+                    b.splice(ii, 1, "_*deleted*_"); // Replace same element with the "_*deleted*_" string.
                 }
             });
         });
-        b.forEach(function (v) {    
-            if (!v.toString().match(/\(\*\)/g)) { // Check that marker. If it isn't there, then...
-                buffer.push(v); // include it in the output (*buffer* variable) array.
+        b.forEach(function (v) {
+            if (!(/_\*deleted\*_/.test(v.toString()))) { // Check the marker.
+                buffer.push(v);
             }
         });
     }
     return buffer;
-};
+}
